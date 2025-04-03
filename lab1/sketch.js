4528f8dad2d9b046c2ce3e655015eea3fac48443
L1xterrY <yatsenkoilarion@gmail.com>
2025-04-03 11:36:43 +0000

    123

---
+++ lab1/sketch.js
@@ -0,0 +1,12 @@
+//код для soul gappy
+
+function setup() {
+    createCanvas(600, 400);
+    background(200);
+}
+
+function draw() {
+    background(200);
+    fill(150, 0, 150);
+    ellipse(mouseX, mouseY, 50, 50);
+} 
